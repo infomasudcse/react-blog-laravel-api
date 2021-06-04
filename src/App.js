d76704e63a5e5685befe85cf35383ed9ab9a6733
Md Masud <infomasud@gmail.com>
2021-06-04 21:33:21 +0600

    Project page set up working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import { Route, Switch } from "react-router";
+import Contact from './pages/Contact';
+import Details from './pages/Details';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Switch>
+        <Route path='/' exact>
+          <Home />
+        </Route>
+        <Route path='/details'>
+          <Details />
+        </Route>
+        <Route path='/contact'>
+          <Contact />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
